5f9e548e3fe267b3cc95944d0c6a4effdc6fb391
Shawn Presser <shawnpresser@gmail.com>
2021-05-12 17:41:24 -0500

    Horrible training loop v1

--- train.py
+++ train.py
@@ -7,7 +7,15 @@ from torchvision import datasets, transforms, utils
 from torchvision.transforms import functional as TF
 from tqdm import tqdm
 
-from vgg_loss import vgg_loss
+# from vgg_loss import vgg_loss
+#
+# "found a much faster/more efficient way to find the minimum distance:
+#
+#   x.pow(2).sum(dim=1, keepdim=True) + y.pow(2).sum(dim=1) - 2 * x @ y.T
+#
+# did this for vqgan type vector quantization which requires you to find all pairwise distances
+# it was *way* faster and *way* less memory usage
+# the expression i quoted gives you all pairwise squared euclidean distances"
 
 BATCH_SIZE = 50
 BIG_BATCH_SIZE = 500
@@ -24,6 +32,10 @@ class ConvBlock(nn.Sequential):
         )
 
 
+class Loss(nn.MSELoss):
+    pass
+
+
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Using device:', device)
@@ -34,26 +46,54 @@ def main():
     train_dl = data.DataLoader(train_set, BATCH_SIZE, shuffle=True,
                                num_workers=1, pin_memory=True)
 
+    # model = nn.Sequential(
+    #     nn.Linear(LATENT_SIZE, 16 * 7 * 7),
+    #     nn.Unflatten(-1, (16, 7, 7)),
+    #     nn.ReLU(inplace=True),
+    #     nn.Upsample(scale_factor=2),
+    #     ConvBlock(16, 16),
+    #     ConvBlock(16, 16),
+    #     nn.Upsample(scale_factor=2),
+    #     ConvBlock(16, 16),
+    #     nn.Conv2d(16, 1, 3, padding=1),
+    #     nn.Sigmoid(),
+    # ).to(device)
+
+    # model = nn.Sequential(
+    #     nn.Conv2d(3, 16, 3, padding=1), nn.ReLU(),
+    #     nn.Conv2d(16, 16, 3, padding=1), nn.ReLU(),
+    #     nn.AvgPool2d(2, ceil_mode=True),
+    #     nn.Conv2d(16, 32, 3, padding=1), nn.ReLU(),
+    #     nn.Conv2d(32, 32, 3, padding=1), nn.ReLU(),
+    #     nn.AvgPool2d(2, ceil_mode=True),
+    #     nn.Conv2d(32, 64, 3, padding=1), nn.ReLU(),
+    #     nn.Conv2d(64, 64, 3, padding=1), nn.ReLU(),
+    #     nn.AvgPool2d(2, ceil_mode=True),
+    #     nn.Flatten(),
+    #     nn.Linear(1024, 128), nn.Tanh(),
+    # ).to(device)
+
     model = nn.Sequential(
-        nn.Linear(LATENT_SIZE, 16 * 7 * 7),
-        nn.Unflatten(-1, (16, 7, 7)),
+        nn.Conv2d(1, 16, 3, padding=1),
         nn.ReLU(inplace=True),
-        nn.Upsample(scale_factor=2),
-        ConvBlock(16, 16),
-        ConvBlock(16, 16),
-        nn.Upsample(scale_factor=2),
-        ConvBlock(16, 16),
-        nn.Conv2d(16, 1, 3, padding=1),
-        nn.Sigmoid(),
+        nn.MaxPool2d(2),
+        nn.Conv2d(16, 16, 3, padding=1),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d(2),
+        nn.Flatten(),
+        nn.Linear(16 * 7 * 7, 10),
     ).to(device)
+
     print('Parameters:', sum(map(lambda x: x.numel(), model.parameters())))
 
-    vgg = vgg_loss.VGGLoss(layer=8).to(device)
+    crit = nn.CrossEntropyLoss()
 
-    def crit(fakes, reals):
-        out = fakes.unsqueeze(0) - reals.unsqueeze(1)
-        out = out.pow(2).mean([2, 3, 4])
-        return out.min(1).values.mean()
+    # vgg = Loss().to(device)
+    #
+    # def crit(fakes, reals):
+    #     out = fakes.unsqueeze(0) - reals.unsqueeze(1)
+    #     out = out.pow(2).mean([2, 3, 4])
+    #     return out.min(1).values.mean()
 
     opt = optim.Adam(model.parameters(), lr=1e-3)
 
@@ -62,34 +102,54 @@ def main():
             model.train()
             losses = []
             i = 0
-            for x, _ in train_dl:
+            # for x, _ in train_dl:
+            #     i += 1
+            #     x = x.to(device, non_blocking=True)
+            #     xf = vgg.get_features(torch.cat([x, x, x], dim=1))
+            #     z = torch.randn([BIG_BATCH_SIZE, LATENT_SIZE], device=device)
+            #     gz = model(z)
+            #     gzf = vgg.get_features(torch.cat([gz, gz, gz], dim=1))
+            #     opt.zero_grad()
+            #     loss = crit(gzf, xf)
+            #     losses.append(loss.item())
+            #     loss.backward()
+            #     opt.step()
+            #     pbar.update(len(x))
+            #     if i % 50 == 0:
+            #         tqdm.write(f'{i * BATCH_SIZE} {sum(losses[-50:]) / 50:g}')
+            #     if i % 500 == 0:
+            #         demo()
+            #         model.train()
+            for inputs, targets in train_dl:
                 i += 1
-                x = x.to(device, non_blocking=True)
-                xf = vgg.get_features(torch.cat([x, x, x], dim=1))
-                z = torch.randn([BIG_BATCH_SIZE, LATENT_SIZE], device=device)
-                gz = model(z)
-                gzf = vgg.get_features(torch.cat([gz, gz, gz], dim=1))
+                inputs = inputs.to(device, non_blocking=True)
+                targets = targets.to(device, non_blocking=True)
+                outputs = model(inputs)
+                loss = crit(outputs, targets)
+                losses.append(loss)
+                # mdmm_return = mdmm_module(loss)
+                # writer.writerow([loss.item(), *(norm.item() for norm in mdmm_return.fn_values)])
                 opt.zero_grad()
-                loss = crit(gzf, xf)
-                losses.append(loss.item())
-                loss.backward()
+                # mdmm_return.value.backward()
                 opt.step()
-                pbar.update(len(x))
+                pbar.update(len(inputs))
                 if i % 50 == 0:
                     tqdm.write(f'{i * BATCH_SIZE} {sum(losses[-50:]) / 50:g}')
-                if i % 500 == 0:
-                    demo()
-                    model.train()
+                # if i % 100 == 0:
+                #     print(f'{i} {sum(losses[-100:]) / 100:g}')
+                #     print('Layer weight norms:',
+                #           *(f'{norm.item():g}' for norm in mdmm_return.fn_values))
 
-    @torch.no_grad()
-    @torch.random.fork_rng()
-    def demo():
-        model.eval()
-        torch.manual_seed(0)
-        z = torch.randn([100, LATENT_SIZE], device=device)
-        grid = utils.make_grid(model(z), 10).cpu()
-        TF.to_pil_image(grid).save('demo.png')
-        tqdm.write('Wrote examples to demo.png.')
+
+    # @torch.no_grad()
+    # @torch.random.fork_rng()
+    # def demo():
+    #     model.eval()
+    #     torch.manual_seed(0)
+    #     z = torch.randn([100, LATENT_SIZE], device=device)
+    #     grid = utils.make_grid(model(z), 10).cpu()
+    #     TF.to_pil_image(grid).save('demo.png')
+    #     tqdm.write('Wrote examples to demo.png.')
 
     def save():
         torch.save({'model': model.state_dict(), 'opt': opt.state_dict()}, PREFIX + '.pth')
@@ -99,7 +159,7 @@ def main():
         for epoch in range(1, EPOCHS + 1):
             print('Epoch', epoch)
             train()
-            demo()
+            # demo()
             save()
     except KeyboardInterrupt:
         pass
